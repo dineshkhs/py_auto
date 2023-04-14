b43e0c77f46ee4e15bfb677d2ff5eac4e66c9c9a
dineshkhs <61654911+dineshkhs@users.noreply.github.com>
2023-04-14 14:32:35 +0100

    Update byteconvertion.py

--- byteconvertion.py
+++ byteconvertion.py
@@ -31,27 +31,27 @@ for repo in repos:
     #clone_cmd = ['git', 'clone', '--depth','1','--branch', repo['commitID'], repo['url'], os.path.join(tmp_dir, repo['url'].split('/')[-1].split('.')[0])]
     
     # Clone the repository
-    subprocess.run(clone_cmd, check=True, shell=True)
+    subprocess.check_call(clone_cmd)
         
 
     os.chdir(os.path.join(repo['url'].split('/')[-1].split('.')[0]))
    
     # Create a virtual environment
-    subprocess.run(['python', '-m', 'venv','--upgrade-deps', '.venv'], check=True)
+    subprocess.check_call(['python', '-m', 'venv','--upgrade-deps', '.venv'])
     
  
     # Activate the virtual environment
     activate_script = os.path.join('.venv', 'bin', 'activate')
 
-    subprocess.run([activate_script, '&&','python', '-m','pip', 'install', 'wheel', 'setuptools', 'versioningit','&&','exit'], check=True, shell=False)
+    subprocess.rucheck_calln([activate_script, '&&','python', '-m','pip', 'install', 'wheel', 'setuptools', 'versioningit','&&','exit'], check=True, shell=False)
 
    
-    subprocess.run([activate_script,'&&','python', '-c', 'from setuptools import setup; setup()','bdist_egg','--exclude-source-files','&&','exit'], shell=False, check=True)
+    subprocess.check_call([activate_script,'&&','python', '-c', 'from setuptools import setup; setup()','bdist_egg','--exclude-source-files','&&','exit'], shell=False, check=True)
     
     
     #subprocess.run(['python', '-c', '"from setuptools import setup; setup()"','bdist_egg','--exclude-source-files'], check=True)
 
-    subprocess.run([activate_script,'&&','python', '-m', 'wheel','convert','dist/*.egg','&&','exit'], shell=False, check=True)
+    subprocess.check_call([activate_script,'&&','python', '-m', 'wheel','convert','dist/*.egg','&&','exit'], shell=False, check=True)
     os.makedirs(os.path.join('..', wheel_dir), exist_ok=True)
     whl_file = os.path.join('.', glob.glob('*.whl')[0])
    # whl_file = os.path.join('.','*.whl')
@@ -60,6 +60,5 @@ for repo in repos:
 # Clean up the temporary directory
 os.chdir('..')
 #shutil.rmtree(tmp_dir)
-
 
 
